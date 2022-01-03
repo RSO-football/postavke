eea6626a462a7e6a0fcbe30e14db88b01af87d17
Rok Bosil <rb2600@student.uni-lj.si>
2022-01-03 17:54:46 +0100

    Changed database

--- models/src/main/resources/sql-scripts/init.sql
+++ models/src/main/resources/sql-scripts/init.sql
@@ -1,2 +1,2 @@
-INSERT INTO postavke_metadata (uporabnikID, pay) VALUES (2, 50);
+INSERT INTO postavke_metadata (uporabnikID, pay) VALUES (1, 50);
 INSERT INTO postavke_metadata (uporabnikID, pay) VALUES (3, 100);
