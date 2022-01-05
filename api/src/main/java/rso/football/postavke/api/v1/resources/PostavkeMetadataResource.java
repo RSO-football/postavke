db9aaf4637375a2b20ca24bbc5f261a730f2b88f
Rok Bosil <rb2600@student.uni-lj.si>
2022-01-05 19:44:34 +0100

    Added openApi

--- api/src/main/java/rso/football/postavke/api/v1/resources/PostavkeMetadataResource.java
+++ api/src/main/java/rso/football/postavke/api/v1/resources/PostavkeMetadataResource.java
@@ -1,6 +1,15 @@
 package rso.football.postavke.api.v1.resources;
 
 import com.kumuluz.ee.cors.annotations.CrossOrigin;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
+import org.eclipse.microprofile.openapi.annotations.headers.Header;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 import rso.football.postavke.lib.PlaceMetadata;
 import rso.football.postavke.lib.PostavkeMetadata;
 import rso.football.postavke.services.beans.PostavkeMetadataBean;
@@ -30,6 +39,13 @@ public class PostavkeMetadataResource {
     @Context
     protected UriInfo uriInfo;
 
+    @Operation(description = "Get all postavke metadata.", summary = "Get all metadata")
+    @APIResponses({
+            @APIResponse(responseCode = "200",
+                    description = "List of postavke metadata",
+                    content = @Content(schema = @Schema(implementation = PostavkeMetadata.class, type = SchemaType.ARRAY)),
+                    headers = {@Header(name = "X-Total-Count", description = "Number of objects in list")}
+            )})
     @GET
     public Response getPostavkeMetadata() {
 
@@ -38,9 +54,17 @@ public class PostavkeMetadataResource {
         return Response.status(Response.Status.OK).entity(postavkeMetadata).build();
     }
 
+    @Operation(description = "Get metadata for one postavka.", summary = "Get metadata for one postavka")
+    @APIResponses({
+            @APIResponse(responseCode = "200",
+                    description = "Igrisce metadata",
+                    content = @Content(
+                            schema = @Schema(implementation = PostavkeMetadata.class))
+            )})
     @GET
     @Path("/{postavkeMetadataId}")
-    public Response getPostavkeMetadata(@PathParam("postavkeMetadataId") Integer postavkeMetadataId) {
+    public Response getPostavkeMetadata(@Parameter(description = "Metadata ID.", required = true)
+                                            @PathParam("postavkeMetadataId") Integer postavkeMetadataId) {
 
         PostavkeMetadata postavkeMetadata = postavkeMetadataBean.getPostavkeMetadata(postavkeMetadataId);
 
@@ -51,6 +75,13 @@ public class PostavkeMetadataResource {
         return Response.status(Response.Status.OK).entity(postavkeMetadata).build();
     }
 
+    @Operation(description = "Get place trenerjev.", summary = "Get place from trenerji")
+    @APIResponses({
+            @APIResponse(responseCode = "200",
+                    description = "Place metadata",
+                    content = @Content(
+                            schema = @Schema(implementation = PlaceMetadata.class))
+            )})
     @GET
     @Path("/place")
     public Response getPlaceMetadata() {
@@ -64,8 +95,18 @@ public class PostavkeMetadataResource {
         return Response.status(Response.Status.OK).entity(placeMetadata).build();
     }
 
+    @Operation(description = "Add postavka metadata.", summary = "Add metadata")
+    @APIResponses({
+            @APIResponse(responseCode = "201",
+                    description = "Metadata successfully added."
+            ),
+            @APIResponse(responseCode = "400", description = "Bad request.")
+    })
     @POST
-    public Response createPostavkeMetadata(PostavkeMetadata postavkeMetadata) {
+    public Response createPostavkeMetadata(@RequestBody(
+            description = "DTO object with postavke metadata.",
+            required = true, content = @Content(
+            schema = @Schema(implementation = PostavkeMetadata.class))) PostavkeMetadata postavkeMetadata) {
         if ((postavkeMetadata.getUporabnikID() == null || new Float(postavkeMetadata.getPay()) == null)) {
             return Response.status(Response.Status.BAD_REQUEST).build();
         }
@@ -83,10 +124,23 @@ public class PostavkeMetadataResource {
 
     }
 
+    @Operation(description = "Update metadata for on postavka.", summary = "Update metadata")
+    @APIResponses({
+            @APIResponse(
+                    responseCode = "204",
+                    description = "Metadata successfully updated."
+            ),
+            @APIResponse(responseCode = "404", description = "Not found.")
+    })
     @PUT
     @Path("{postavkeMetadataId}")
-    public Response putPostavkeMetadata(@PathParam("postavkeMetadataId") Integer postavkeMetadataId,
-                                     PostavkeMetadata postavkeMetadata) {
+    public Response putPostavkeMetadata(@Parameter(description = "Metadata ID.", required = true)
+                                            @PathParam("postavkeMetadataId") Integer postavkeMetadataId,
+                                        @RequestBody(
+                                                description = "DTO object with postavka metadata.",
+                                                required = true, content = @Content(
+                                                schema = @Schema(implementation = PostavkeMetadata.class)))
+                                                PostavkeMetadata postavkeMetadata) {
 
 //        if (postavkeMetadata.getUporabnikID() != null && postavkeMetadataBean.existsPostavakaTrenerja(postavkeMetadata.getUporabnikID())){
 //            return Response.status(Response.Status.BAD_REQUEST).build();
@@ -102,9 +156,21 @@ public class PostavkeMetadataResource {
 
     }
 
+    @Operation(description = "Delete metadata for one postavka.", summary = "Delete metadata")
+    @APIResponses({
+            @APIResponse(
+                    responseCode = "204",
+                    description = "Metadata successfully deleted."
+            ),
+            @APIResponse(
+                    responseCode = "404",
+                    description = "Not found."
+            )
+    })
     @DELETE
     @Path("{postavkeMetadataId}")
-    public Response deletePostavkeMetadata(@PathParam("postavkeMetadataId") Integer postavkeMetadataId) {
+    public Response deletePostavkeMetadata(@Parameter(description = "Metadata ID.", required = true)
+                                               @PathParam("postavkeMetadataId") Integer postavkeMetadataId) {
 
         boolean deleted = postavkeMetadataBean.deletePostavkeMetadata(postavkeMetadataId);
 
